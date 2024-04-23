f0e5674c27ef3af7bd8d8a3530444e262ccb2dec
AmirZiaee1 <Amirziaee22@gmail.com>
2024-04-23 15:10:21 +0200

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'; 
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+createRoot(document.getElementById('root')).render( 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
